8fd467062903175f939207bd80c938000375d1b3
AlexMart <alejandromartpv@icloud.com>
2023-11-10 20:16:52 -0300

    update: (main.js) events functions & optimizations

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,29 +1,39 @@
 const DisplayRoot = document.getElementById("displayRoot");
 
-
-const canvas = document.getElementById("videocall-canvas");
-
-setTimeout(() => {
-    
-    if(canvas) {
-       MainRender().renderToTarget(canvas);
-    }
-}, 1500);
-
+const events = {}
 
 window.addEventListener("message", function(ev) {
-	const data = ev.data;
-
-	//if (data.display != undefined) DisplayRoot.classList.toggle("hidden", !data.display);
+	const eventData = ev.data;
+    const id = eventData.id
+    const duiName = eventData.duiName
+    const eventName = eventData.eventName
+    const content = eventData.content
+    emitEvent(duiName, eventName, id, content)
 });
 
+function AddEventListener(duiName, eventName, callback) {
+    if (!events[duiName]) {
+        events[duiName] = {};
+    }
 
+    if (!events[duiName][eventName]) {
+        events[duiName][eventName] = [];
+    }
+
+    events[duiName][eventName].push(callback);
+}
+
+function emitEvent(duiName, eventName, id, data) {
+    if (events[duiName] && events[duiName][eventName]) {
+        events[duiName][eventName].forEach(callback => {
+            callback(id, data);
+        });
+    }
+}
 
 
 // Due to loading order, we need to let the resource side know when *we* are ready.
 // Just having the browser "loaded" isn't enough.
-
-
 function MarkDUIAsReady(duiName){
     fetch(`https://${document.location.host}/duiIsReady`, {
         method: 'POST',
