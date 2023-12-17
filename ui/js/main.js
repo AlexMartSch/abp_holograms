6fac5e3d8ede90a33e8a5e6c830b2dc6147e80e9
AlexMart <alejandromartpv@icloud.com>
2023-12-17 00:33:29 -0300

    add: remote callback js

--- ui/js/main.js
+++ ui/js/main.js
@@ -31,9 +31,24 @@ function emitEvent(duiName, eventName, id, data) {
     }
 }
 
+function SendData(duiName, eventName, content) {
+    fetch('https://' + document.location.host + '/sendData', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({
+            id: duiName,
+            eventName,
+            content
+        })
+    })
+}
 
 // Due to loading order, we need to let the resource side know when *we* are ready.
 // Just having the browser "loaded" isn't enough.
+
+
 function MarkDUIAsReady(duiName){
     fetch(`https://${document.location.host}/duiIsReady`, {
         method: 'POST',
